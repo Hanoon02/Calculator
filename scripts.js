e131ab1260bb088e42e21f7941402d6c86a4c1a9
Hanoon02 <ahemdhanoon02@gmail.com>
2022-07-13 12:58:57 +0530

    Added Button Functionality

--- scripts.js
+++ scripts.js
@@ -1,8 +1,25 @@
 function add(){
-    document.getElementById('Add').addEventListener("click"); 
+    document.getElementById('Add');//.addEventListener("click"); 
     var number1 = window.prompt("Enter the First Number: ");
     var number2 = window.prompt("Enter the Second Number: ");
-    alert("The numbers added together are: " + (parseInt(number1) + parseInt(number2)));
+    alert("The sum is: " + (parseInt(number1) + parseInt(number2)));
 };
-
-alert("Hello World!");
+function subtract(){
+    document.getElementById('Add');//.addEventListener("click"); 
+    var number1 = window.prompt("Enter the First Number: ");
+    var number2 = window.prompt("Enter the Second Number: ");
+    alert("The difference is: " + (parseInt(number1) - parseInt(number2)));
+};
+function multiply(){
+    document.getElementById('Add');//.addEventListener("click");
+    var number1 = window.prompt("Enter the First Number: ");
+    var number2 = window.prompt("Enter the Second Number: ");
+    alert("The product is: " + (parseInt(number1) * parseInt(number2)));
+}
+function divide(){
+    document.getElementById('Add');//.addEventListener("click");
+    var number1 = window.prompt("Enter the First Number: ");
+    var number2 = window.prompt("Enter the Second Number: ");
+    alert("The quotient is: " + (parseInt(number1) / parseInt(number2)));
+}
+//alert("Hello World!");
