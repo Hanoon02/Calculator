61fd5a6e755078af7f5c103f60013ac23617f0a3
Hanoon02 <ahmedhanoon02@gmail.com>
2022-07-15 21:26:07 +0530

    Added functionality to numbers

--- scripts.js
+++ scripts.js
@@ -1,21 +1,100 @@
+const zero = document.getElementById("zero");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+let number1=0;
+let number2=0;
+
 function add(){
-    var number1 = window.prompt("Enter the First Number: ");
-    var number2 = window.prompt("Enter the Second Number: ");
     alert("The sum is: " + (parseInt(number1) + parseInt(number2)));
 };
 function subtract(){
-    var number1 = window.prompt("Enter the First Number: ");
-    var number2 = window.prompt("Enter the Second Number: ");
     alert("The difference is: " + (parseInt(number1) - parseInt(number2)));
 };
 function multiply(){
-    var number1 = window.prompt("Enter the First Number: ");
-    var number2 = window.prompt("Enter the Second Number: ");
     alert("The product is: " + (parseInt(number1) * parseInt(number2)));
 }
 function divide(){
-    var number1 = window.prompt("Enter the First Number: ");
-    var number2 = window.prompt("Enter the Second Number: ");
     alert("The quotient is: " + (parseInt(number1) / parseInt(number2)));
 }
-//alert("Hello World!");
+
+function assignNumber(num){
+    if(number1==0){
+        number1 = num;
+    }
+    else{
+        number2 = num;
+    }
+}
+function clear(){
+    number1 = 0;
+    number2 = 0;
+}
+
+one.addEventListener("click");
+{
+    event.target.style.backgroundColor = 'red';
+    assignNumber(1);
+}
+two.addEventListener("click");
+{
+    assignNumber(2);
+}
+three.addEventListener("click");
+{
+    assignNumber(3);
+}
+four.addEventListener("click");
+{
+    assignNumber(4);
+}
+five.addEventListener("click");
+{
+    assignNumber(5);
+}
+six.addEventListener("click");
+{
+    assignNumber(6);
+}
+seven.addEventListener("click");
+{
+    assignNumber(7);
+}
+eight.addEventListener("click");
+{
+    assignNumber(8);
+}
+nine.addEventListener("click");
+{
+    assignNumber(9);
+}
+zero.addEventListener("click");
+{
+    assignNumber(0);
+}
+
+add.addEventListener("click");
+{
+    add();
+}
+subtract.addEventListener("click");
+{
+    subtract();
+}
+multiply.addEventListener("click");
+{
+    multiply();
+}
+divide.addEventListener("click");
+{
+    divide();
+}
+
+// Language: javascript
+// Path:
